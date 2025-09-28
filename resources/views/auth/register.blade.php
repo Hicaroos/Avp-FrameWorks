e49e910041585498c908c76982c47a79154d9aa3
Hicaroos <hicaro9182@gmail.com>
2025-09-28 13:49:16 -0300

    Criado Request para login e register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,6 +3,7 @@
     <input type="text" name="name" placeholder="Nome" required>
     <input type="email" name="email" placeholder="Email" required>
     <input type="password" name="password" placeholder="Senha" required>
+    <input type="password" name="password_confirmation" placeholder="Confirme a senha" required>
     <button type="submit">Registrar</button>
 </form>
 
